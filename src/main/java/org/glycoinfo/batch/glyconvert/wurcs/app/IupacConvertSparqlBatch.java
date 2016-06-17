28570bdf98d960cc6d191f4c00701137ce09e580
aoki <aoki@bluetree.jp>
2016-06-17 10:50:20 +0900

    fix component scan

--- src/main/java/org/glycoinfo/batch/glyconvert/wurcs/app/IupacConvertSparqlBatch.java
+++ src/main/java/org/glycoinfo/batch/glyconvert/wurcs/app/IupacConvertSparqlBatch.java
@@ -1,11 +1,14 @@
 package org.glycoinfo.batch.glyconvert.wurcs.app;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = ("org.glycoinfo.batch.rdf"))
+@EnableBatchProcessing
 public class IupacConvertSparqlBatch {
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
