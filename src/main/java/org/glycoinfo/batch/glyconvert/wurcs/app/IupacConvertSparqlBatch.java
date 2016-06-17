0a1a1550aa0d96575a3d3ecb0b2a9eb190dfa573
aoki <aoki@bluetree.jp>
2016-06-17 11:12:49 +0900

    add all spring boot, batch configuration

--- src/main/java/org/glycoinfo/batch/glyconvert/wurcs/app/IupacConvertSparqlBatch.java
+++ src/main/java/org/glycoinfo/batch/glyconvert/wurcs/app/IupacConvertSparqlBatch.java
@@ -1,13 +1,21 @@
 package org.glycoinfo.batch.glyconvert.wurcs.app;
 
+import org.glycoinfo.rdf.dao.virt.VirtSesameTransactionConfig;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @ComponentScan(basePackages = ("org.glycoinfo.batch.rdf"))
+@EnableBatchProcessing
+@Import(VirtSesameTransactionConfig.class)
 public class IupacConvertSparqlBatch {
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
